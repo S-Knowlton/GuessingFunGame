0d54cf96dc2641718f30841a40958157ba532ba6
Unknown <SKnowlton@student.neumont.edu>
2018-08-08 17:33:05 -0600

    Added layout array

--- app/src/main/java/com/clas/guessingfungame/MainActivity.java
+++ app/src/main/java/com/clas/guessingfungame/MainActivity.java
@@ -2,12 +2,15 @@ package com.clas.guessingfungame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Layout[] questions = new Layout[10];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
